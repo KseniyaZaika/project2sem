96dcd36ae2c4938787f5cfecc70903e7d4b00dc4
KseniyaZaika <113699296+KseniyaZaika@users.noreply.github.com>
2023-06-13 04:00:56 +0200

    дописано передвижение
    разбила класс player на файлы, есть реф в текстурах на карту, вот можешь через аррей попробовать сгенерить ее и коллизии в идеале еще бы сразу тулз разбить на хедер с спп

--- Player.cpp
+++ Player.cpp
@@ -1,8 +1,69 @@
 #include "Player.h"
-//Player::Player(float i_x, float i_y)
-//{
-//	x = i_x;
-//	y = i_y;
-//	direction_horizontal = 0;
-//	direction_vertical = 0;
-//}
+#include "GlobalSizes.h"
+#include "Tools.h"
+
+Player::Player(float _x, float _y)
+{
+    x = _x; y = _y;
+    player_texture.loadFromFile("D:/repos/proj2/from/textures/MapPlayer16.png");
+    player_sprite.setTexture(player_texture);
+    size = player_texture.getSize().y;
+}
+
+void Player::set_position(float _x, float _y)
+{
+    x = _x;
+    y = _y;
+}
+
+void Player::update(const sf::RenderWindow& i_window)
+{
+    // Right-Left
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+    {
+        direction += ROTATION_SPEED;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+    {
+        direction -= ROTATION_SPEED;
+    }
+
+    //WASD
+    direction = GetDegrees(direction);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+    {
+        x += MOVEMENT_SPEED * cos(DegToRad(GetDegrees(90 + direction)));
+        y -= MOVEMENT_SPEED * sin(DegToRad(GetDegrees(90 + direction)));
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+    {
+        x += MOVEMENT_SPEED * cos(DegToRad(GetDegrees(direction - 90)));
+        y -= MOVEMENT_SPEED * sin(DegToRad(GetDegrees(direction - 90)));
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+    {
+        x -= MOVEMENT_SPEED * cos(DegToRad(GetDegrees(direction)));
+        y += MOVEMENT_SPEED * sin(DegToRad(GetDegrees(direction)));
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+    {
+        x += MOVEMENT_SPEED * cos(DegToRad(GetDegrees(direction)));
+        y -= MOVEMENT_SPEED * sin(DegToRad(GetDegrees(direction)));
+    }
+
+    direction = fmod(360 + fmod(direction, 360), 360);//fmod-остаток от деления в формате числа с плавающей точкой ( функция приведения диапазона угла)
+}
+
+void Player::draw(sf::RenderWindow& i_window)
+{
+    float frame_angle = 360.f / (player_texture.getSize().x / size);
+    float shifted_direction = fmod(360 + fmod(direction + 0.5f * frame_angle, 360), 360);
+
+    player_sprite.setPosition(x, y);
+
+    player_sprite.setTextureRect(sf::IntRect(size * floor(shifted_direction / frame_angle), 0, size, size));
+    //player_sprite.setTextureRect(sf::IntRect(0,0,32,32));
+    i_window.draw(player_sprite);
+}
+
+
--- Player.h
+++ Player.h
@@ -1,19 +1,23 @@
 #pragma once
+#include <windows.h>
+#include <vector>
+#include <SFML\Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <cmath>
 
-//class Player
-//{
-//	float x; //стартовая позиция
-//	float y;
-//
-//	float direction_horizontal; // поворот головы
-//	float direction_vertical;
-//
-//public:
-//	Player(float i_x, float i_y);
-//
-//	void update()
-//	{
-//		if 
-//	}
-//
-//};
+class Player // 
+{
+    float x;
+    float y;
+    float direction = 0;
+    sf::Sprite player_sprite;
+    sf::Texture player_texture;
+    float size;
+
+public:
+    Player(float _x, float _y);
+    void set_position(float _x, float _y);
+    void update(const sf::RenderWindow& i_window);
+    void draw(sf::RenderWindow& i_window);
+    
+};
